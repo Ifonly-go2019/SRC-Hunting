2d9ddffe77a8c0fb9e57090e2dc6a73e1dc2962f
M0nk3y <52337707+Ifonly-go2019@users.noreply.github.com>
2020-04-22 10:30:28 +0800

    完善将结果保存至文件
    结果在脚本同目录的results.txt中，修复一个脚本运行结束报错

--- EDU-domain-crawler/EDU-domain-crawler.py
+++ EDU-domain-crawler/EDU-domain-crawler.py
@@ -1,10 +1,12 @@
-# coding=UTF-8
 # author: M0nk3y
 # 功能实现：爬取所有学校网站的主站
 # Data：2020/04/21
 # 根据 http://www.hao123.com/edu  写一个爬虫，爬取所有大学的官方网站
+# coding: UTF-8
 import requests
 import bs4
+import time
+import os
 
 logo = """
 ___________________   ____ ___          ________                        .__                 ________                      .__
@@ -15,6 +17,8 @@ ___________________   ____ ___          ________                        .__     
         \/         \/                           \/             \/     \/        \/                  \/           \/                 \/
 """
 print(logo)
+print("[+]------------Loading----------[+]")
+time.sleep(1)
 url = 'http://www.hao123.com/edu'
 r = requests.get(url)
 rSoup = bs4.BeautifulSoup(r.text, 'html.parser')
@@ -27,18 +31,27 @@ for i in range(32):
         if 'htm' not in k['href']:
             continue
         else:
-            a = k['href']
-            r = requests.get(a)
-            rSoup = bs4.BeautifulSoup(r.text, 'html.parser')
-            for g in rSoup.find_all('a'):
-                if 'baidu' in g['href']:
-                    continue
-                if 'sina' in g['href']:
-                    continue
-                if 'hao123' in g['href']:
-                    continue
-                if 'javascript' in g['href']:
-                    continue
-                else:
-                    print(g['href'])
-    print("[+]-------------ALL Down![+]------------")
+            try:
+                a = k['href']
+                r = requests.get(a)
+                rSoup = bs4.BeautifulSoup(r.text, 'html.parser')
+                for g in rSoup.find_all('a'):
+                    if 'baidu' in g['href']:
+                        continue
+                    if 'sina' in g['href']:
+                        continue
+                    if 'hao123' in g['href']:
+                        continue
+                    if 'javascript' in g['href']:
+                        continue
+                    else:
+                        a = g['href']
+                        print(a)
+                        file_handle = open('result.txt', 'a')
+                        file_handle.write(a)
+                        file_handle.write('\n')
+                        file_handle.close()
+            except KeyError:
+                pass
+
+print("[+]-------------ALL Down！------------[+]")
